f14ab13945b925d4b565983b71cbce3712b19724
asquaredigital <asquaredigitalsolutions@gmail.com>
2023-10-12 23:38:42 +0530

    Update contact2.php

--- contact2.php
+++ contact2.php
@@ -24,7 +24,7 @@ $message = "Name: $u_name\nEmail: $u_email\nPhone Number: $p_number\nCompany Nam
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if (mail('iecfabchemwebsite@gmail.com', $subject, $message, $headers)) {
+if (mail('elavarasan5193@gmail.com', $subject, $message, $headers)) {
     // Email sent successfully
     $response = array('message' => 'Email sent successfully!');
     echo json_encode($response);
